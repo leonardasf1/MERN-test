b1db2610ae2b0ae833ed480785a3078957d91831
leonardasf1 <leonardasf1@yandex.ru>
2020-08-17 15:30:51 +0300

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+## testing stack: Mongo Express React Node
+
+This project client:
+ 
+ [Create React App](https://github.com/facebook/create-react-app),
+ [Materialize](https://materializecss.com/),
+ [react-router-dom](https://www.npmjs.com/package/react-router-dom)
+ 
+This project server:
+
+[Express](https://expressjs.com/),
+[Mongoose](https://mongoosejs.com/),
+[config](https://lorenwest.github.io/node-config/),
+[bcrypt.js](https://github.com/dcodeIO/bcrypt.js#readme),
+[express-validator](https://express-validator.github.io/docs/),
+[jsonwebtoken](https://github.com/auth0/node-jsonwebtoken#readme),
+[nodemon](https://nodemon.io/),
+[Concurrently](https://github.com/kimmobrunfeldt/concurrently#readme),
+[shortid](https://github.com/dylang/shortid#readme),
+[cross-env](https://www.npmjs.com/package/cross-env)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
---
+++ client/src/pages/CreatePage.js
@@ -0,0 +1,39 @@
+import React, {useContext, useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContext";
+
+export const CreatePage = () => {
+  const history = useHistory()
+  const auth = useContext(AuthContext)
+  const {request} = useHttp()
+  const [link, setLink] = useState('')
+  const pressHandler = async event => {
+    if (event.key === 'Enter') {
+      try {
+        const data = await request('/api/link/generate', 'POST', {from: link}, {
+          Authorization: `Bearer ${auth.token}`
+        })
+        history.push(`/detail/${data.link._id}`)
+      } catch (e) {}
+
+    }
+  }
+    return (
+        <div className="row">
+            <div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
+              <div className="input-field">
+                <input
+                  id="link"
+                  type="text"
+                  name="link"
+                  value={link}
+                  onChange={e => setLink(e.target.value)}
+                  onKeyPress={pressHandler}
+                />
+                <label htmlFor="first_name">Вставьте ссылку</label>
+              </div>
+            </div>
+        </div>
+    )
+}
